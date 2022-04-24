1ae12dfe8690f9b76b86269a8268d4c94e8a9df1
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-24 15:21:05 +0600

    created a api with get method and mongo

--- index.js
+++ index.js
@@ -9,11 +9,22 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wcxgg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
-    client.connect()
-    const productCollection = await client.db("emaJhon").collection("products");
-    app.get('/', (req, res) => {
-        res.send('Hello crazy World!')
-    })
+    try {
+        client.connect()
+        const productCollection = client.db("emaJhon").collection("products");
+        app.get('/', (req, res) => {
+            res.send('Hello crazy World!')
+        })
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+    }
+    finally {
+
+    }
 }
 run().catch(console.dir)
 app.listen(port, () => {
