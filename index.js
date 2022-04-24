7ebf817ca908a00241c9b625082100368d808fa3
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-24 14:42:05 +0600

    all settings to index js done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello crazy World!')
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
