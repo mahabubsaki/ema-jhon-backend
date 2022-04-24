83142c1494d62e654d09bdf69f8e947cc8b24ce1
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-04-24 14:59:29 +0600

    setup mongodb

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ const app = express()
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
-app.get('/', (req, res) => {
-    res.send('Hello crazy World!')
-})
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wcxgg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    client.connect()
+    const productCollection = await client.db("emaJhon").collection("products");
+    app.get('/', (req, res) => {
+        res.send('Hello crazy World!')
+    })
+}
+run().catch(console.dir)
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
